169343edfa80a5035a4d6aa6aec3c3582d0a7448
Matthew Kwong <matthewkwong2@gmail.com>
2021-10-10 14:45:31 +0800

    [Test] Performance

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,6 +1,6 @@
 // import Contact from "async!./Contact";
-import Footer from "async!./Footer";
-import Landing from "async!./Landing";
+import Footer from "./Footer";
+import Landing from "./Landing";
 import NavBar from "./NavBar";
 
 const Home = () => (
