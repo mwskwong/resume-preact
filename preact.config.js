f76a7b635e48a7fd82002dae8517986d3b4cd2bd
Matthew Kwong <matthewkwong2@gmail.com>
2021-10-10 15:33:33 +0800

    [Test] Use emotion

--- preact.config.js
+++ preact.config.js
@@ -4,9 +4,9 @@ export default {
   ],
   webpack: (config, env) => {
     config.resolve.modules.push(env.src);
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@mui/styled-engine": "@mui/styled-engine-sc"
-    };
+    // config.resolve.alias = {
+    //   ...config.resolve.alias,
+    //   "@mui/styled-engine": "@mui/styled-engine-sc"
+    // };
   }
 };
